6b28d32a51356e8b664758666348509bf10dbe3e
Carolene Wilson <carolenesw@gmail.com>
2019-12-21 21:20:43 -0500

    add variables to js file

--- code.js
+++ code.js
@@ -2,123 +2,33 @@
 
   var quizQuestions = [
     {
-      title: "Commonly used data types DO NOT include:",
+      question: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
+      question: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
     {
-      title: "What is the HTML tag under which one can write the JavaScript code.",
+      question: "What is the HTML tag under which one can write the JavaScript code.",
       choices: ["javascript", "scripted", "script", "js"],
       answer: "script"
     },
     {
-      title: "The external JavaScript file must contain <script> tag.",
+      question: "The external JavaScript file must contain <script> tag.",
       choices: ["true", "false", "true with quotes", "false with quotes"],
       answer: "false"
     },
     {
-      title: " Which of the following is not a reserved word in JavaScript",
+      question: " Which of the following is not a reserved word in JavaScript",
       choices: ["interface", "throws", "program", "short"],
       answer: "program"
     }
   ];
 
-
-
-  function Quiz(questions) {
-    this.score = 0;
-    this.questions = questions;
-    this.questionIndex = 0;
-}
- 
-Quiz.prototype.getQuestionIndex = function() {
-    return this.questions[this.questionIndex];
-}
- 
-Quiz.prototype.guess = function(answer) {
-    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-        this.score++;
-    }
- 
-    this.questionIndex++;
-}
- 
-Quiz.prototype.isEnded = function() {
-    return this.questionIndex === this.questions.length;
-}
- 
- 
-function Question(text, choices, answer) {
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
-}
- 
-Question.prototype.isCorrectAnswer = function(choice) {
-    return this.answer === choice;
-}
- 
- 
-function populate() {
-    if(quiz.isEnded()) {
-        showScores();
-    }
-    else {
-        // show question
-        var element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
- 
-        // show options
-        var choices = quiz.getQuestionIndex().choices;
-        for(var i = 0; i < choices.length; i++) {
-            var element = document.getElementById("choice" + i);
-            element.innerHTML = choices[i];
-            guess("btn" + i, choices[i]);
-        }
- 
-        showProgress();
-    }
-};
- 
-function guess(id, guess) {
-    var button = document.getElementById(id);
-    button.onclick = function() {
-        quiz.guess(guess);
-        populate();
-    }
-};
- 
- 
-function showProgress() {
-    var currentQuestionNumber = quiz.questionIndex + 1;
-    var element = document.getElementById("progress");
-    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
-};
- 
-function showScores() {
-    var gameOverHTML = "<h1>Result</h1>";
-    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-    var element = document.getElementById("quiz");
-    element.innerHTML = gameOverHTML;
-};
- 
-// create questions here
-var questions = [
-    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
-    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
-    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
-    new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
-];
- 
-// create quiz
-var quiz = new Quiz(questions);
- 
-// display quiz
-populate();
+  var userStatus = 0;
+  var correctAnswers = 0;
+  var progress, quiz, question, choices, answer, ansA, ansB, ansC, ansD;
   
