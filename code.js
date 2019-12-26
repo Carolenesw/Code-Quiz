775a3de9a6b3ddbfc23c7634b3d8ceb05197c021
Carolene Wilson <carolenesw@gmail.com>
2019-12-26 00:35:55 -0500

    add questions.js with quiz questions and answers

--- code.js
+++ code.js
@@ -9,37 +9,10 @@ var userStatus = 0;
 var correctAnswers = 0;
 var progress, quiz, question, choices, answer, ansA, ansB, ansC;
 
-//quiz questions, choices and answer in array for selection. 
 
-  var quizQuestions = [
-    {
-      question: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts"],
-      answer: "alerts"
-    },
-    {
-      question: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      question: "What is the HTML tag under which one can write the JavaScript code.",
-      choices: ["scripted", "script", "js"],
-      answer: "script"
-    },
-    {
-      question: "The external JavaScript file must contain <script> tag.",
-      choices: ["true", "false", "false with quotes"],
-      answer: "false"
-    },
-    {
-      question: " Which of the following is not a reserved word in JavaScript",
-      choices: ["interface", "throws", "program"],
-      answer: "program"
-    }
-  ];
 
   var startQuiz = confirm("Start Quiz");
+
 
 function quizQuestions(choices = choices[0]) {
   if (quizQuestions === "question[0]") {
@@ -48,6 +21,7 @@ function quizQuestions(choices = choices[0]) {
     console.log(error);
   }
 }
+
 
 console.log(quizQuestions);
   
---
+++ questions.js
@@ -0,0 +1,29 @@
+//quiz questions, choices and answer in array for selection. 
+
+var quizQuestions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts"],
+    answer: "alerts"
+  },
+  {
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    question: "What is the HTML tag under which one can write the JavaScript code.",
+    choices: ["scripted", "script", "js"],
+    answer: "script"
+  },
+  {
+    question: "The external JavaScript file must contain <script> tag.",
+    choices: ["true", "false", "false with quotes"],
+    answer: "false"
+  },
+  {
+    question: " Which of the following is not a reserved word in JavaScript",
+    choices: ["interface", "throws", "program"],
+    answer: "program"
+  }
+];
