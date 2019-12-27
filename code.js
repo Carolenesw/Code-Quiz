006ab006b3b67baf9af0f7d14821716d4c06caa3
Carolene Wilson <carolenesw@gmail.com>
2019-12-26 22:45:58 -0500

    add button var using get element by id function

--- code.js
+++ code.js
@@ -2,6 +2,9 @@
 
 var progressA = document.getElementById("progress");
 var quizQes = document.getElementById("quiz");
+var buttOne = document.getElementById("btn0");
+var buttTwo = document.getElementById("btn1");
+var buttThr = document.getElementById("btn2");
 
 // add variables for for the questions and status of the user 
 
@@ -10,18 +13,39 @@ var correctAnswers = 0;
 var progress, quiz, question, choices, answer, ansA, ansB, ansC;
 
 
-
   var startQuiz = confirm("Start Quiz");
 
+//quiz questions, choices and answer in array for selection. 
 
-function quizQuestions(choices = choices[0]) {
-  if (quizQuestions === "question[0]") {
-    return alert
-  } else {
-    console.log(error);
-  }
-}
-
-
-console.log(quizQuestions);
+var quizQuestions = {
   
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts"],
+    answer: "alerts"
+  ,
+  
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "parentheses", "square brackets"],
+    answer: "parentheses"
+  ,
+  
+    question: "What is the HTML tag under which one can write the JavaScript code.",
+    choices: ["scripted", "script", "js"],
+    answer: "script"
+  ,
+  
+    question: "The external JavaScript file must contain <script> tag.",
+    choices: ["true", "false", "false with quotes"],
+    answer: "false"
+  ,
+  
+    question: " Which of the following is not a reserved word in JavaScript",
+    choices: ["interface", "throws", "program"],
+    answer: "program"
+
+};
+
+
+
+  alert("Question 1" + quizQuestions["question1"]);
+//  alert ("Question 1" + quizQuestions["question1"]);
--- questions.js
+++ questions.js
@@ -2,28 +2,29 @@
 
 var quizQuestions = [
   {
-    question: "Commonly used data types DO NOT include:",
+    question1: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts"],
     answer: "alerts"
   },
   {
-    question: "The condition in an if / else statement is enclosed within ____.",
+    question2: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "parentheses", "square brackets"],
     answer: "parentheses"
   },
   {
-    question: "What is the HTML tag under which one can write the JavaScript code.",
+    question3: "What is the HTML tag under which one can write the JavaScript code.",
     choices: ["scripted", "script", "js"],
     answer: "script"
   },
   {
-    question: "The external JavaScript file must contain <script> tag.",
+    question4: "The external JavaScript file must contain <script> tag.",
     choices: ["true", "false", "false with quotes"],
     answer: "false"
   },
   {
-    question: " Which of the following is not a reserved word in JavaScript",
+    question5: " Which of the following is not a reserved word in JavaScript",
     choices: ["interface", "throws", "program"],
     answer: "program"
   }
 ];
+
