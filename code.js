d87dd40f339728c51b7a33eb24ca3cab6a95c760
Carolene Wilson <carolenesw@gmail.com>
2019-12-28 14:15:47 -0500

    make correction to array

--- code.js
+++ code.js
@@ -20,33 +20,33 @@ var progress, quiz, question, choices, answer, ansA, ansB, ansC;
 
 //quiz questions, choices and answer in array for selection. 
 
-var quizQuestions = {
+var quizQuestions = [
   
-    question: "Commonly used data types DO NOT include:",
+   { question: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts"],
     answer: "alerts"
-  ,
-  
+},
+  {
     question: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "parentheses", "square brackets"],
     answer: "parentheses"
-  ,
-  
+  },
+  {
     question: "What is the HTML tag under which one can write the JavaScript code.",
     choices: ["scripted", "script", "js"],
     answer: "script"
-  ,
+  },
   
-    question: "The external JavaScript file must contain <script> tag.",
+ { question: "The external JavaScript file must contain <script> tag.",
     choices: ["true", "false", "false with quotes"],
     answer: "false"
-  ,
+  },
   
-    question: " Which of the following is not a reserved word in JavaScript",
+   { question: " Which of the following is not a reserved word in JavaScript",
     choices: ["interface", "throws", "program"],
     answer: "program"
-
-};
+    }
+];
 
 function runTimer() {
   var timeInterval = setInterval(function()
@@ -72,3 +72,5 @@ function sendMessage() {
 runTimer();
 
 console.log(runTimer());
+
+
