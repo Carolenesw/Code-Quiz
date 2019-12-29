0e3a69dc9a8911b5ed916792df59d0e87803ce00
Carolene Wilson <carolenesw@gmail.com>
2019-12-28 20:31:58 -0500

    reformat quiz questions and answer plus variables

--- code.js
+++ code.js
@@ -1,13 +1,12 @@
 //create all necessary elements. 
 
-var progressA = document.getElementById("progress");
-var comQuiz = document.getElementById("main")
+const progressA = document.getElementById("progress");
+var subQuiz = document.getElementById("submit")
 var quizQes = document.getElementById("quiz");
-var buttOne = document.getElementById("btn0");
-var buttTwo = document.getElementById("btn1");
-var buttThr = document.getElementById("btn2");
+var quizResuts = document.getElementById("result");
 
 var timeLeft = 75;
+
 
 // add variables for for the questions and status of the user 
 
@@ -16,61 +15,112 @@ var correctAnswers = 0;
 var progress, quiz, question, choices, answer, ansA, ansB, ansC;
 
 
-  var startQuiz = confirm("Start Quiz");
+var startQuiz = confirm("Start Quiz");
 
-//quiz questions, choices and answer in array for selection. 
 
-var quizQuestions = [
-  
-   { question: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts"],
-    answer: "alerts"
-},
+// quiz display and results
+function beginQuiz() {
+
+}
+
+function results() {}
+
+
+beginQuiz();
+
+subQuiz.addEventListener('click', results);
+
+// quiz questions, choices and answer for selection. 
+
+var quiQuestions = [
   {
-    question: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "parentheses", "square brackets"],
-    answer: "parentheses"
+    question: "Commonly used data types DO NOT include?",
+    choices: {
+      a: "strings",
+      b: "booleans",
+      c: "alerts"
+    },
+    Answer: "c"
   },
+
   {
-    question: "What is the HTML tag under which one can write the JavaScript code.",
-    choices: ["scripted", "script", "js"],
-    answer: "script"
+    question: "The condition in an if or else statement is enclosed within ____.?",
+    choices: {
+      a: "quotes",
+      b: "parentheses",
+      c: "square brackets"
+    },
+    Answer: "b"
   },
   
- { question: "The external JavaScript file must contain <script> tag.",
-    choices: ["true", "false", "false with quotes"],
-    answer: "false"
+  {
+    question: "What is the HTML tag under which one can write the JavaScript code.?",
+    choices: {
+      a: "scripted",
+      b: "script",
+      c: "js"
+    },
+    Answer: "b"
   },
-  
-   { question: " Which of the following is not a reserved word in JavaScript",
-    choices: ["interface", "throws", "program"],
-    answer: "program"
-    }
+
+  {
+    question: "The external JavaScript file must contain <script> tag?",
+    choices: {
+      a: "true",
+      b: "false",
+      c: "true with quotes"
+    },
+    Answer: "b"
+  },
+
+  {
+    question: "Which of the following is not a reserved word in JavaScript?",
+    choices: {
+      a: "interface",
+      b: "throw",
+      c: "program"
+    },
+    Answer: "c"
+  },
 ];
 
-function runTimer() {
-  var timeInterval = setInterval(function()
-  {
-    secondsLeft--;
-    progressA.textContent = secondsLeft + "time left to complete quiz.";
-
-    if(secondsLeft === 0) {
-      clearInterval(timeInterval);
-      sendMessage();
-    }
-
-  }, 75000);
-}
-function sendMessage() {
-  progressA.textContent = " ";
-
-  var butFinish = document.createElement("button");
-  butFinish.textContent = "Quiz Complete!";
-  comQuiz.appendChild(butFinish);
-}
-
-runTimer();
-
-console.log(runTimer());
 
 
+
+  
+  
+ 
+  
+   
+// function runTimer() {
+//   var timeInterval = setInterval(function()
+//   {
+//     secondsLeft --;
+//     progressA.textContent = secondsLeft + "time left to complete quiz.";
+
+//     if(secondsLeft === 0) {
+//       clearInterval(timeInterval);
+//       sendMessage();
+//     }
+
+//   }, 75000);
+// }
+// function sendMessage() {
+//   progressA.textContent = " ";
+
+//   var butFinish = document.createElement("button");
+//   butFinish.textContent = "Quiz Complete!";
+//   comQuiz.appendChild(butFinish);
+// }
+
+// runTimer();
+
+// console.log(runTimer());
+
+// function array() {
+//   if(myArray === myArray[0]);
+//   chioces[0];
+// // } if (myArray === myArray[1]); {
+// //   choices[1];
+// // }
+// array();
--- questions.js
+++ questions.js
@@ -1,30 +1,51 @@
-//quiz questions, choices and answer in array for selection. 
+var quiQuestions = [
+  {
+    question: "Commonly used data types DO NOT include?",
+    choices: {
+      a: "strings",
+      b: "booleans",
+      c: "alerts"
+    },
+    Answer: "c"
+  },
 
-var quizQuestions = [
   {
-    question1: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts"],
-    answer: "alerts"
+    question: "The condition in an if or else statement is enclosed within ____.?",
+    choices: {
+      a: "quotes",
+      b: "parentheses",
+      c: "square brackets"
+    },
+    Answer: "b"
   },
+  
   {
-    question2: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "parentheses", "square brackets"],
-    answer: "parentheses"
+    question: "What is the HTML tag under which one can write the JavaScript code.?",
+    choices: {
+      a: "scripted",
+      b: "script",
+      c: "js"
+    },
+    Answer: "b"
   },
+
   {
-    question3: "What is the HTML tag under which one can write the JavaScript code.",
-    choices: ["scripted", "script", "js"],
-    answer: "script"
+    question: "The external JavaScript file must contain <script> tag?",
+    choices: {
+      a: "true",
+      b: "false",
+      c: "true with quotes"
+    },
+    Answer: "b"
   },
+
   {
-    question4: "The external JavaScript file must contain <script> tag.",
-    choices: ["true", "false", "false with quotes"],
-    answer: "false"
+    question: "Which of the following is not a reserved word in JavaScript?",
+    choices: {
+      a: "interface",
+      b: "throw",
+      c: "program"
+    },
+    Answer: "c"
   },
-  {
-    question5: " Which of the following is not a reserved word in JavaScript",
-    choices: ["interface", "throws", "program"],
-    answer: "program"
-  }
 ];
-
