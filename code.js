979ef60974bc5d686d718a10116578f99b217a69
Carolene Wilson <carolenesw@gmail.com>
2019-12-30 18:04:24 -0500

    reformat timer and add onclick function to js page

--- code.js
+++ code.js
@@ -86,45 +86,68 @@ var quiQuestions = [
   },
 ];
 
-var quiQuestions1 = $("quiQuestions");
+// var quiQuestions1 = $("quiQuestions");
 
-for (var i = 0; i < quiQuestions.length; i++) {
-  var newQuizQues = $("")
-}
+// for (var i = 0; i < quiQuestions.length; i++) {
+//   var newQuizQues = $("")
+// }
 
 
 //set timer seconds to run quiz 
-var today = new Date();
-var runTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var maxTime = 75000;
+// var today = new Date();
+// var runTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var maxTime = 75000;
 
-var timer = Date.parse(maxTime) - Date.parse(new Date());
-var seconds = Math.floor((runTime/1000) * 15);
-  var minutes = Math.floor((runTime/1000/60) * 60);
+// var timer = Date.parse(maxTime) - Date.parse(new Date());
+// var seconds = Math.floor((runTime/1000) * 15);
+//   var minutes = Math.floor((runTime/1000/60) * 60);
 
-function getTimeLeft(maxTime) {
-return { 
-  "minutes": minutes,
-  "seconds" : seconds
-};
-}
-console.log(getTimeLeft());
-getTimeLeft(runTime).seconds
+// function getTimeLeft(maxTime) {
+// return { 
+//   "minutes": minutes,
+//   "seconds" : seconds
+// };
+// }
+// console.log(getTimeLeft());
+// getTimeLeft(runTime).seconds
 
-// set timer output to run time in seconds 
-var progressA = document.getElementById(progress);
+// // set timer output to run time in seconds 
+// var progressA = document.getElementById(progress);
 
-function startTimer(id, maxTime) {
-  var timerInterval = setInterval (function() {
-  var timer = getTimeLeft(maxTime);
-  progress.innerHTML = "minutes:"  + timer.second + "seconds: " + timer.seconds;
+// function startTimer(id, maxTime) {
+//   var timerInterval = setInterval (function() {
+//   var timer = getTimeLeft(maxTime);
+//   progress.innerHTML = "minutes:"  + timer.second + "seconds: " + timer.seconds;
   
-  if(timer.total == 0) {
-    clearInterval(timerInterval);
-  }
-  }, 1000);
+//   if(timer.total == 0) {
+//     clearInterval(timerInterval);
+//   }
+//   }, 1000);
   
-}
-console.log(startTimer("id"));  
-startTimer(progress, runTime);
+// }
+// console.log(startTimer("id"));  
+// startTimer(progress, runTime);
+var timerId;
+var timeLeft = 75;
 
+   
+
+$("#Start").on("click", function () {
+  alert("Start Quiz!")
+
+       timerId = setInterval(countdown, 1000);  
+    function countdown() {            
+      if (timeLeft === -1) {                
+        clearTimeout(timerId);                
+      }            
+      else {                
+        $("#rem-time").text(timeLeft);               
+         timeLeft--;            }        
+        }        
+                
+    
+        });
+
+        function resultCount(){
+          timeLeft = 30;
+        }
